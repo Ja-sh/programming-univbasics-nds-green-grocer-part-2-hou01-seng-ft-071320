9d726560c652f63157cb38a5bd466de84f6f0f21
Joshua Moore <morejoshua99@hotmail.com>
2020-07-12 02:13:51 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,31 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
-def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+def apply_coupons (cart, coupons)
+  i = 0
+  cart.each do |k|
+    coupons.each do |coups|
+      
+      if k[:item] == coups[:item] && k[:count] == coups[:num]
+       cart << {:item =>"#{coups[:item]} W/COUPON" , 
+         :price => coups[:cost]/coups[:num], 
+         :clearance => k[:clearance],
+           :count => coups[:num]}
+        k[:count] = k[:count] - coups[:num]
+      
+  end
+end
+end
 end
 
 def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
-  # REMEMBER: This method **should** update cart
+  # REMEMBER: This method **should** update consolidate_cart
+  cart.each do |k|
+    if k[:clearance] == true
+      cart = k[:price] - 0.20
+binding.pry
 end
 
 def checkout(cart, coupons)
